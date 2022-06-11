83f6bfb0dae3db429292ce6e1c1574ff65e3de0b
Zaffias <rdpp08@gmail.com>
2022-06-11 20:56:03 +0200

    frot end added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ morgan.token('data', (req,res) => JSON.stringify(req.body))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
-
+app.use(express.static('build'))
 app.get('/api/persons',(req,res) => {
     res.json(data)
 })
